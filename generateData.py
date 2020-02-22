344c6e7b3004a2fc12301975a6b18f257af03e42
roshjcm <roshane.jochum@mail.mcgill.ca>
2020-02-22 17:20:37 -0500

    Done generating data for user and created csv file

--- generateData.py
+++ generateData.py
@@ -1,6 +1,7 @@
 import numpy as np 
-import random
+from random import randint, choice
 import csv
+import pandas
 
 
 num= 50 #num of instances to be generated
@@ -11,29 +12,45 @@ first_names=['Louise','Janna','Roshane', 'Kyra','Olive','Soraya','Tara','Misgana
 last_names=['Wang','Jones','Porta','Kourilsky','Garcia','Patel','Nguyen','Meijer','Markoski',\
 			'Wilson','Williams','Rodriguez','D\'Angelo','Leone','Castelli']
 countries=['Canada','United States','Mexico','France','Egypt','Bolivia','Vietnam','China','Italy','Russia','England','Spain']
+email_tails=['@gmail.com',"@yahoo.com","@aol.com","@mail.mcgill.ca"]
 
 name_list=[]
 username_list=[]
+email_list=[]
 country_list=[]
 birthday_list=[]
 credit_info_list=[]
 uid_list=[]
 
 for i in range(num):
-	name=random.choice(first_names)+" "+random.choice(last_names)
+	first=choice(first_names)
+	last=choice(last_names)
+	name=first+" "+last
 	name_list.append(name)
-	country_list.append(random.choice(countries))
-	username= name[0:2]+str(randint(0,400))
-	username_list.append()
+
+	email= first+"."+last+choice(email_tails)
+	email_list.append(email)
+
+	country_list.append(choice(countries))
+
+	username= name[0:3]+str(randint(0,400))
+	username_list.append(username)
+
 	birthday=str(randint(1950,2007))+"-"+str(randint(1,12))+"-"+str(randint(1,29))
 	birthday_list.append(birthday)
+
 	credit_info=str(randint(1111111111111111,9999999999999999)) #16 digits credit card info
 	credit_info_list.append(credit_info)
+
 	uid= randint(0,400)
 	while (uid in uid_list):
 		uid=randint(0,400)
 	uid_list.append(uid)
 
+# with open('user.csv', 'w') as myfile:
+#      wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+#      wr.writecolumn(mylist)
 
-
-
+df = pandas.DataFrame(data={"uid": uid_list, "name": name_list, "username": username_list, "country":country_list,"email":email_list, 
+	"birthday":birthday_list,"credit info":credit_info_list})
+df.to_csv("./user.csv", sep=',',index=False)
