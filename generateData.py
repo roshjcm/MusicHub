90973c8bf6b028bcec99afd431aeacecdbff0042
roshjcm <roshane.jochum@mail.mcgill.ca>
2020-02-22 17:01:57 -0500

    Modifying structure and generating credit info

--- generateData.py
+++ generateData.py
@@ -9,26 +9,24 @@ first_names=['Louise','Janna','Roshane', 'Kyra','Olive','Soraya','Tara','Misgana
 				'Maanav', 'Thomas', 'Sam', 'Karim', 'Max', 'Elio', 'Theo', 'Kian', 'Zhang', 'Magnus']
 last_names=['Wang','Jones','Porta','Kourilsky','Garcia','Patel','Nguyen','Meijer','Markoski',\
 			'Wilson','Williams','Rodriguez','D\'Angelo','Leone','Castelli']
+countries=['Canada','United States','Mexico','France','Egypt','Bolivia','Vietnam','China','Italy','Russia','England','Spain']
 
 name_list=[]
+username_list=[]
+country_list=[]
+birthday_list=[]
+credit_info_list=[]
+
 for i in range(num):
 	name=random.choice(first_names)+" "+random.choice(last_names)
 	name_list.append(name)
-
-print(name_list)
-
-
-username_list=[]
-for i in range(num):
-	username= name_list[i][0:2]+str(randint(0,400))
-	username_list.append()
-
-countries=['Canada','United States','Mexico','France','Egypt','Bolivia','Vietnam','China','Italy','Russia','England','Spain']
-country_list
-for i in range(num):
 	country_list.append(random.choice(countries))
-
-birthday_list=[]
-for i in range(num):
+	username= name[0:2]+str(randint(0,400))
+	username_list.append()
 	birthday=str(randint(1950,2007))+"-"+str(randint(1,12))+"-"+str(randint(1,29))
 	birthday_list.append(birthday)
+	credit_info=str(randint(1111111111111111,9999999999999999)) #16 digits credit card info
+	credit_info_list.append(credit_info)
+	
+
+
