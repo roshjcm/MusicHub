7304d4f83faae8b6cd4bd3c38c734c4edadb5a3e
roshjcm <roshane.jochum@mail.mcgill.ca>
2020-02-22 16:32:09 -0500

    generating random names

--- generateData.py
+++ generateData.py
@@ -1,5 +1,14 @@
-import pydbgen		#
+import numpy as np 
+import random
 
 
-myDB=pydbgen.pydb()
+num= 50 #num of instances to be generated
+first_names=['Louise','Janna','Roshane', 'Kyra','Olive','Soraya','Tara','Misgana','Noreen', 'Bahar'
+				'Maanav', 'Thomas', 'Sam', 'Karim', 'Max', 'Elio', 'Theo', 'Kian', 'Zhang', 'Magnus']
+last_names=['Wang','Jones','Porta','Kourilsky','Garcia','Patel','Nguyen','Meijer','Markoski',\
+			'Wilson','Williams','Rodriguez','D\'Angelo','Leone','Castelli']
 
+name=random.choice(first_names)+" "+random.choice(last_names)
+name_list=name*num
+
+print(name_list)
