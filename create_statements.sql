865ad185bc78bc6c9821f3628d973aa6a3c39666
Louoyar <oyarzabal.louise@gmail.com>
2020-02-23 15:33:07 -0500

    added relation sets in create_statements.sql

--- create_statements.sql
+++ create_statements.sql
@@ -1,4 +1,4 @@
-
+/*Entity Sets*/
 
 CREATE TABLE users (
 	uid INTEGER NOT NULL,
@@ -62,6 +62,56 @@ CREATE TABLE shopping_carts(
 	num_articles INT,
 	PRIMARY KEY (order_id)
 );
+
+/*Relation sets*/
+
+CREATE TABLE Releases ( 
+	FOREIGN KEY(artist_id) REFERENCES artists(uid),
+	FOREIGN KEY(article_id) REFERENCES articles
+);
+
+CREATE TABLE BelongsTo ( 
+	FOREIGN KEY(song_id) REFERENCES songs(article_id),
+	FOREIGN KEY(album_id) REFERENCES albums(article_id)
+);
+
+CREATE TABLE ComprisesOf ( 
+	FOREIGN KEY(lib_id) REFERENCES libraries,
+	FOREIGN KEY(name) REFERENCES playlists
+);
+
+CREATE TABLE IsAddedTo ( 
+	FOREIGN KEY(articl_id) REFERENCES articles,
+	FOREIGN KEY(lib_id) REFERENCES libraries
+);
+
+CREATE TABLE IsPartOf ( 
+	FOREIGN KEY(name) REFERENCES playlist,
+	FOREIGN KEY(lib_id) REFERENCES libraries
+);
+
+CREATE TABLE Contains ( 
+	FOREIGN KEY(order_id) REFERENCES shopping_carts,
+	FOREIGN KEY(article_id) REFERENCES articles
+);
+
+CREATE TABLE Uses ( 
+	FOREIGN KEY(listener_id) REFERENCES listeners(uid),
+	FOREIGN KEY(order_id) REFERENCES shopping_carts
+);
+
+CREATE TABLE Creates ( 
+	FOREIGN KEY(listener_id) REFERENCES listeners(uid),
+	FOREIGN KEY(lib_id) REFERENCES libraries,
+	FOREIGN KEY(name) REFERENCES playlist
+);
+
+CREATE TABLE moneyEarned ( 
+	FOREIGN KEY(artist_id) REFERENCES artists(uid),
+	FOREIGN KEY(order_id) REFERENCES shopping_carts,
+	money_received FLOAT (4)
+);
+
 
 
 
--- queries.sql
+++ queries.sql
@@ -1,38 +1,38 @@
 /* Display the content of an Album*/
 SELECT S.title, S.release_date, S.genre, S.price
-FROM Song S
-WHERE S.articleID IN (SELECT B.songID
-				FROM BelongsTo B, Album A
-				WHERE A.articleID = B.albumID )
+FROM songs S
+WHERE S.article_id IN (SELECT B.song_id
+				FROM BelongsTo B, albums A
+				WHERE A.article_id = B.album_id )
 
 /* Search all the Artist releases*/
 SELECT title, release_date, genre, price
-FROM Article
-WHERE articleID IN (SELECT R.songID
-				FROM Releases R, Artist A
-				WHERE A.userID = R.artistID)
+FROM articles
+WHERE article_id IN (SELECT R.song_id
+				FROM Releases R, artist A
+				WHERE A.user_id = R.artist_id)
 
 /* Get all the Articles that the Listener selected in his shopping cart*/
 SELECT DISTINCT Article.id
-FROM Article
-WHERE articleID IN (SELECT C.articleID
-				FROM Contains C, ShoppingCart S
-				WHERE S.orderID = C.orderID)
+FROM articles
+WHERE article_id IN (SELECT C.articleID
+				FROM Contains C, shopping_carts S
+				WHERE S.order_id = C.order_id)
 
 /* Display all the songs from a specific artist in the Listener's Library*/
 SELECT S.title
-FROM Song S
+FROM songs S
 WHERE S.articleID IN (SELECT R.songID
 				FROM Releases R, Artist A
 				WHERE A.userID = R.artistID)
 INTERSECT
 SELECT S.title
-FROM Song S
-WHERE S.articleID IN (SELECT A.articleID
-				FROM Library L, isAdded A
+FROM songs S
+WHERE S.article_id IN (SELECT A.article_id
+				FROM libraries L, IsAddedTo A
 				WHERE A.libid = L.libid)
 
 /*Display the Listener's order history*/
-SELECT Uses.orderID
-FROM Uses, Listener
-WHERE Uses.listenerID = Listener.userID
+SELECT Uses.order_id
+FROM Uses, listeners
+WHERE Uses.listener_Iid = listeners.uid
