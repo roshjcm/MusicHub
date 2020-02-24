4daa2203045da9fa3b3b5cfd28553e384fa3f6b0
roshjcm <roshane.jochum@mail.mcgill.ca>
2020-02-24 17:09:30 -0500

    Finishing the Article dataset + main function for generateData

--- generateData.py
+++ generateData.py
@@ -3,9 +3,6 @@ from random import randint, choice
 import csv
 import pandas
 
-
-num= 50 #num of instances to be generated
-
 #*************************************** USERS *********************************************************
 first_names=['Louise','Janna','Roshane', 'Kyra','Olive','Soraya','Tara','Misgana','Noreen', 'Bahar',\
 				'Maanav', 'Thomas', 'Sam', 'Karim', 'Max', 'Elio', 'Theo', 'Kian', 'Zhang', 'Magnus']
@@ -22,38 +19,65 @@ birthday_list=[]
 credit_info_list=[]
 uid_list=[]
 
-for i in range(num):
-	first=choice(first_names)
-	last=choice(last_names)
-	name=first+" "+last
-	name_list.append(name)
+def generateUserData(num):
+	for i in range(num):
+		first=choice(first_names)
+		last=choice(last_names)
+		name=first+" "+last
+		name_list.append(name)
 
-	email= first+"."+last+choice(email_tails)
-	email_list.append(email)
+		email= first+"."+last+choice(email_tails)
+		email_list.append(email)
 
-	country_list.append(choice(countries))
+		country_list.append(choice(countries))
 
-	username= name[0:3]+str(randint(0,400))
-	username_list.append(username)
+		username= name[0:3]+str(randint(0,400))
+		username_list.append(username)
 
-	birthday=str(randint(1950,2007))+"-"+str(randint(1,12))+"-"+str(randint(1,29))
-	birthday_list.append(birthday)
+		birthday=str(randint(1950,2007))+"-"+str(randint(1,12))+"-"+str(randint(1,29))
+		birthday_list.append(birthday)
 
-	credit_info=str(randint(1111111111111111,9999999999999999)) #16 digits credit card info
-	credit_info_list.append(credit_info)
+		credit_info=str(randint(1111111111111111,9999999999999999)) #16 digits credit card info
+		credit_info_list.append(credit_info)
 
-	uid= randint(0,400)
-	while (uid in uid_list):
-		uid=randint(0,400)
-	uid_list.append(uid)
+		uid= randint(0,400)
+		while (uid in uid_list):
+			uid=randint(0,400)
+		uid_list.append(uid)
 
-# with open('user.csv', 'w') as myfile:
-#      wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-#      wr.writecolumn(mylist)
 
-df = pandas.DataFrame(data={"uid": uid_list, "name": name_list, "username": username_list, "country":country_list,"email":email_list, 
-	"birthday":birthday_list,"credit info":credit_info_list})
-df.to_csv("./datasets/user.csv", sep=',',index=False)
+	df_users = pandas.DataFrame(data={"uid": uid_list, "name": name_list, "username": username_list, "country":country_list,"email":email_list, 
+		"birthday":birthday_list,"credit info":credit_info_list})
+	df_users.to_csv("./datasets/user.csv", sep=',',index=False)
 
 
 
+def generateArticleID():
+	# Now need to add articleID to the article.csv file
+	df_articles= pandas.read_csv("./datasets/articles.csv", index_col=False, encoding='ISO-8859-1')
+	print(np.shape(df_articles))
+	articleID_list=[]
+	for i in range(149):
+		articleID= randint(0,500)
+		while (articleID in articleID_list):
+			articleID=randint(0,500)
+		articleID_list.append(articleID)
+	df_articles['articleID']=articleID_list
+	df_articles.to_csv('./datasets/articles.csv', index=False)
+
+def main_menu():
+	answer=-1
+	print("Main Menu")
+	while (not(answer==1 or answer==2)):
+		print("Enter 1 to generate User data \nEnter 2 to generate Article IDs\n")
+		answer=int(input())
+	if (answer==1):
+		print("How many users do you want?")
+		num= int(input())
+		generateUserData(num)
+	else:
+		generateArticleID()
+
+
+
+main_menu()
