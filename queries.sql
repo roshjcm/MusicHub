7ddaf10020f67e0d96e2809f6bf5319489c24cb3
Louoyar <oyarzabal.louise@gmail.com>
2020-02-26 16:44:50 -0500

    some modifications..again

--- queries.sql
+++ queries.sql
@@ -3,7 +3,6 @@ SELECT *
 FROM songs, BelongsTo
 WHERE songs.article_id = BelongsTo.song_id AND BelongsTo.album_id = "album_id_of_interest";
 
-
 --Search all the releases of specific artist--
 SELECT title
 FROM articles
@@ -12,11 +11,18 @@ WHERE article_id IN (SELECT song_id
 				WHERE artist_id = "artist_id_of_interest");
 
 --Get all the Articles that the Listener selected in his shopping cart--
-SELECT DISTINCT Article.id
+SELECT DISTINCT title, price
 FROM articles
-WHERE article_id IN (SELECT C.articleID
-				FROM Contains C
-				WHERE S.order_id = C.order_id);
+WHERE EXISTS (SELECT article_id
+				FROM Contains
+				WHERE order_id = "shopping_cart_id_of_interest");
+
+--get the total number of songs bought for each artist in a specific order --
+SELECT r.artist_id, COUNT(a.article_id)
+FROM articles a, Releases r
+WHERE a.article_id = r.article_id
+	AND a.article_id = ANY (SELECT article_id FROM Contains WHERE order_id = "orderid_of_interest")
+GROUP BY artist_id;
 
 --Display all the songs from a specific artist in the Listener's Library--
 SELECT S.title
@@ -29,17 +35,25 @@ WHERE S.article_id = A.article_id AND A.libid = "lib_id_of_interest";
 
 --Display the Listener's order history--
 SELECT Uses.order_id
-FROM Uses,
+FROM Uses
 WHERE Uses.listener_id = "listener_id_of_interest";
 
--- find the  artist who made more than 20 releases in january--
+-- Ideas for the creativity part--
 
-CREATE VIEW [january selection] AS
+-- find the artists who made more than 20 releases in january 2020--
+
+CREATE VIEW january_selection AS
 SELECT A.article_id, R.artist_id
 FROM Releases R, articles A
 WHERE R.article_id = A.article_id AND 2020-01-01 <= A.release_date < 2020-02-01;
 
 SELECT artists.stage_name
 FROM artists
-WHERE artists.uid EXISTS (SELECT COUNT(article_id), A.uid FROM [january selection] GROUP BY A.uid) AND COUNT(article_id)>20;
+WHERE EXISTS (	SELECT COUNT(article_id), A.uid 
+				FROM january_selection
+				GROUP BY A.uid 
+				HAVING COUNT(article_id) >20); 
+
+DROP VIEW IF EXISTS january_selection;
+
 
