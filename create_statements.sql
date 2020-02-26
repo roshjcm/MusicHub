7f5f86d7ca5007f2e6c7538742a4655d67487a67
Louoyar <oyarzabal.louise@gmail.com>
2020-02-26 17:10:06 -0500

    correction relation sets

--- create_statements.sql
+++ create_statements.sql
@@ -70,50 +70,78 @@ CREATE TABLE shopping_carts(
 /* Relation sets */
 
 CREATE TABLE Releases ( 
+	aritst_id INTEGER NOT NULL,
+	article_id INTEGER NOT NULL,
+	PRIMARY KEY(artists_id, article_id),
 	FOREIGN KEY(artist_id) REFERENCES artists(uid),
 	FOREIGN KEY(article_id) REFERENCES articles
 );
 
 CREATE TABLE BelongsTo ( 
+	song_id INTEGER NOT NULL,
+	album_id INTEGER NOT NULL,
+	PRIMARY KEY(song_id, album_id),
 	FOREIGN KEY(song_id) REFERENCES songs(article_id),
 	FOREIGN KEY(album_id) REFERENCES albums(article_id)
 );
 
 CREATE TABLE ComprisesOf ( 
+	lib_id INTEGER NOT NULL,
+	name VARCHAR(30),
+	PRIMARY KEY(lib_id, name),
 	FOREIGN KEY(lib_id) REFERENCES libraries,
 	FOREIGN KEY(name) REFERENCES playlists
 );
 
 CREATE TABLE IsAddedTo ( 
-	FOREIGN KEY(articl_id) REFERENCES articles,
+	article_id INTEGER NOT NULL,
+	lib_id INTEGER NOT NULL,
+	PRIMARY KEY(article_id, lib_id),
+	FOREIGN KEY(article_id) REFERENCES articles,
 	FOREIGN KEY(lib_id) REFERENCES libraries
 );
 
-CREATE TABLE IsPartOf ( 
+CREATE TABLE IsPartOf (
+	name VARCHAR(30),
+	lib_id INTEGER NOT NULL,
+	PRIMARY KEY(name, lib_id),
 	FOREIGN KEY(name) REFERENCES playlist,
 	FOREIGN KEY(lib_id) REFERENCES libraries
 );
 
 CREATE TABLE Contains ( 
+	order_id INTEGER NOT NULL, 
+	article_id INTEGER NOT NULL,
+	PRIMARY KEY(order_id, article_id),
 	FOREIGN KEY(order_id) REFERENCES shopping_carts,
 	FOREIGN KEY(article_id) REFERENCES articles
 );
 
 CREATE TABLE Uses ( 
+	listener_id INTEGER NOT NULL,
+	order_id INTEGER NOT NULL, 
+	PRIMARY KEY(listener_id, order_id),
 	FOREIGN KEY(listener_id) REFERENCES listeners(uid),
 	FOREIGN KEY(order_id) REFERENCES shopping_carts
 );
 
 CREATE TABLE Creates ( 
+	listener_id INTEGER NOT NULL,
+	lib_id INTEGER NOT NULL,
+	name VARCHAR(30),
+	PRIMARY KEY(listener_id, lib_id, name),
 	FOREIGN KEY(listener_id) REFERENCES listeners(uid),
 	FOREIGN KEY(lib_id) REFERENCES libraries,
 	FOREIGN KEY(name) REFERENCES playlist
 );
 
 CREATE TABLE moneyEarned ( 
+	aritst_id INTEGER NOT NULL,
+	order_id INTEGER NOT NULL, 
+	money_received FLOAT (4),
+	PRIMARY KEY(aritst_id, order_id),
 	FOREIGN KEY(artist_id) REFERENCES artists(uid),
-	FOREIGN KEY(order_id) REFERENCES shopping_carts,
-	money_received FLOAT (4)
+	FOREIGN KEY(order_id) REFERENCES shopping_carts
 );
 
 
