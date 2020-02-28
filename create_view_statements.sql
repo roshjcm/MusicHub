ee7a67a06712c2720e18b9f979da797fa7505698
Louoyar <oyarzabal.louise@gmail.com>
2020-02-27 19:55:00 -0500

    one view added

--- create_view_statements.sql
+++ create_view_statements.sql
@@ -4,8 +4,20 @@
 /* songs that are also albums (singles) */
 
 
-CREATE VIEW singles ()
-AS SELECT * 
-FROM albums article_id
-WHERE albums.type='single'
+CREATE VIEW singles ()AS
+SELECT * 
+FROM albums article
+WHERE albums.type='single';
+
+-- create view of the top 10 artists so far--
+
+CREATE VIEW Top10Artists AS
+SELECT art.stage_name, COUNT(a.article_id)
+FROM isAddedTo A, releases R, artists art
+WHERE A.article_id = R.article_id AND art.uid = R.artist_id
+GROUP BY art.stage_name
+ORDER BY COUNT(a.article_id) DESC 
+LIMIT 10;
+
+
 
