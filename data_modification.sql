878080a67b02740af304388ad90c03b74f3d9d7e
Louoyar <oyarzabal.louise@gmail.com>
2020-02-28 19:25:50 -0500

    q6

--- data_modification.sql
+++ data_modification.sql
@@ -8,3 +8,10 @@ WHERE uid IN (SELECT r.artist_id
 	GROUP BY r.artist_id
 	HAVING COUNT(r.article_id)>5) ;
 
+--give a 10$ discount to all orders more than 3.50$--
+
+UPDATE shopping_carts
+SET total_amount = total_amount*0.9
+WHERE total_amount > 3.50;
+
+--
